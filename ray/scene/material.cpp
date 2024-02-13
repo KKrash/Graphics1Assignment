de9a45e7741f247e9d5ff80145df43a33df6e444
kphang2 <karahphang@gmail.com>
2024-02-12 23:04:50 -0600

    textureMapping done not cubeMapping lol

--- ray/scene/material.cpp
+++ ray/scene/material.cpp
@@ -67,10 +67,12 @@ glm::dvec3 TextureMap::getMappedValue(const glm::dvec2 &coord) const {
   // and use these to perform bilinear interpolation
   // of the values.
 
+  double x = coord[0] * width;
+  double y = coord[1] * height;
   int x_value = std::floor(coord[0]*width);
   int y_value = std::floor(coord[1]*height);
-
-  glm::dvec3 pixelColorMain = getPixelAt(x_value, y_value);
+  int x_value2 = (x_value < width) ? x_value+1 : x_value;
+  int y_value2 = (y_value < height) ? y_value+1 : y_value;
 
   // by default, it's going to be 
   /*
@@ -79,68 +81,57 @@ glm::dvec3 TextureMap::getMappedValue(const glm::dvec2 &coord) const {
     - x, y + 1
     - x + 1, y + 1
   */
-  glm::dvec3 pixelBeneath;
-  glm::dvec3 pixelRight;
-  glm::dvec3 pixelDiag;
 
   // worst case, bottom right corner
-  if (x_value == width && y_value == height)
-  {
-    int x_value2 = x_value-1;
-    int y_value2 = y_value-1;
-    // technically pixelLeft but shhhhh
-    pixelRight = getPixelAt(x_value2, y_value);
-    pixelDiag = getPixelAt(x_value2, y_value2);
-  }
-  else
-  {
-    pixelBeneath = (y_value == height) ? getPixelAt(x_value, y_value-1) : getPixelAt(x_value, y_value+1);
-    pixelRight = (x_value == width) ? getPixelAt(x_value-1, y_value) : getPixelAt(x_value+1, y_value);
-    if(x_value == width)
-    {
-      pixelDiag = getPixelAt(x_value-1, y_value+1);
-    }
-    else if (y_value == height)
-    {
-      pixelDiag = getPixelAt(x_value+1, y_value-1);
-    }
-    else
-    {
-      pixelDiag = getPixelAt(x_value+1, y_value+1);
-    }
-  }
-
-  glm::dvec3 bilinearValues = interpolationOfColors(pixelColorMain, pixelBeneath, pixelRight, pixelDiag, coord);
-
-  return bilinearValues;
-}
-
-glm::dvec3 TextureMap::interpolationOfColors(glm::dvec3 main, glm::dvec3 below, glm::dvec3 right, glm::dvec3 diagonal, const glm::dvec2 &coord) const
-{
-    // with the bottom and diagonal
-  double red1 = (below[0]*((diagonal.x - coord[0])/(diagonal.x - below.x))) + (diagonal[0]*((coord[0] - below.x)/(diagonal.x - below.x)));
-  double green1 = (below[1]*((diagonal.x - coord[0])/(diagonal.x - below.x))) + (diagonal[1]*((coord[0] - below.x)/(diagonal.x - below.x)));
-  double blue1 = (below[2]*((diagonal.x - coord[0])/(diagonal.x - below.x))) + (diagonal[2]*((coord[0] - below.x)/(diagonal.x - below.x)));
-
-  glm::dvec3 inter1 = glm::dvec3(red1, green1, blue1);
-
-  // with the main and the right pixel
-  double red2 = (main[0]*((right.x - coord[0])/(right.x - main.x))) + (right[0]*((coord[0] - main.x)/(right.x - main.x)));
-  double green2 = (main[1]*((right.x - coord[0])/(right.x - main.x))) + (right[1]*((coord[0] - main.x)/(right.x - main.x)));
-  double blue2 = (main[2]*((right.x - coord[0])/(right.x - main.x))) + (right[2]*((coord[0] - main.x)/(right.x - main.x)));
-
-  glm::dvec3 inter2 = glm::dvec3(red2, green2, blue2);
-
-  // putting the two inters together
+  double q1 = (x_value2 - x) / (x_value2 - x_value);
+  double q2 = (x - x_value) / (x_value2 - x_value);
   
-  double red3 = (inter1[0]*((inter2.y-coord[1])/(inter2.y-inter1.y))) + (inter2[0]*((coord[1]-inter1.y)/(inter2.y-inter1.y)));
-  double green3 = (inter1[1]*((inter2.y-coord[1])/(inter2.y-inter1.y))) + (inter2[1]*((coord[1]-inter1.y)/(inter2.y-inter1.y)));
-  double blue3 = (inter1[2]*((inter2.y-coord[1])/(inter2.y-inter1.y))) + (inter2[2]*((coord[1]-inter1.y)/(inter2.y-inter1.y)));
+  // bilinear interpolation! Thanks! I hate it!
+  glm::dvec3 Q12 = getPixelAt(x_value, y_value); // top left
+  glm::dvec3 Q11 = getPixelAt(x_value, y_value2); // bottom left
+  glm::dvec3 Q22 = getPixelAt(x_value2, y_value); // top right
+  glm::dvec3 Q21 = getPixelAt(x_value2, y_value2); // bottom right
 
-  glm::dvec3 finalInterpolation = glm::dvec3(red3, green3, blue3);
-  return finalInterpolation;
+  glm::dvec3 R1 = (Q11*q1) + (Q21*q2);
+  glm::dvec3 R2 = (Q12*q1) + (Q22*q2);
 
+  double r1 = (y_value2 - y) / (y_value2 - y_value);
+  double r2 = (y - y_value) / (y_value2 - y_value);
+
+  glm::dvec3 P = (R1*r1) + (R2*r2);
+
+  //double xcoord = (x_value2 < x_value) ? (x_value - x_value2) : (x_value2 - x_value);
+
+  // double red1 = (pixelBeneath[0]*((x_value2 - coord[0])/(xcoord))) + (pixelDiag[0]*((coord[0] - x_value)/(xcoord)));
+  // double green1 = (pixelBeneath[1]*((x_value2- coord[0])/(xcoord))) + (pixelDiag[1]*((coord[0] - x_value)/(xcoord)));
+  // double blue1 = (pixelBeneath[2]*((x_value2 - coord[0])/(xcoord))) + (pixelDiag[2]*((coord[0] - x_value)/(xcoord)));
+
+  // glm::dvec3 inter1 = glm::dvec3(red1, green1, blue1);
+
+  // // with the main and the right pixel
+  // double red2 = (pixelColorMain[0]*((x_value2 - coord[0])/(xcoord))) + (pixelRight[0]*((coord[0] - x_value)/(xcoord)));
+  // double green2 = (pixelColorMain[1]*((x_value2 - coord[0])/(xcoord))) + (pixelRight[1]*((coord[0] - x_value)/(xcoord)));
+  // double blue2 = (pixelColorMain[2]*((x_value2 - coord[0])/(xcoord))) + (pixelRight[2]*((coord[0] - x_value)/(xcoord)));
+
+  //glm::dvec3 inter2 = glm::dvec3(red2, green2, blue2);
+
+  // // putting the two inters together
+  // double ycoord = (y_value2 < y_value) ? (y_value - y_value2) : (y_value2 - y_value);
+
+  // double red3 = (inter1[0]*((y_value2-coord[1])/(ycoord))) + (inter2[0]*((coord[1]-y_value)/(ycoord)));
+  // double green3 = (inter1[1]*((y_value2-coord[1])/(ycoord))) + (inter2[1]*((coord[1]-y_value)/(ycoord)));
+  // double blue3 = (inter1[2]*((y_value2-coord[1])/(ycoord))) + (inter2[2]*((coord[1]-y_value)/(ycoord)));
+
+  // glm::dvec3 bilinearValues = glm::dvec3(red3, green3, blue3);
+  // bilinearValues[0] = bilinearValues[0] < 0 ? 0 : bilinearValues[0];
+  // bilinearValues[1] = bilinearValues[1] < 0 ? 0 : bilinearValues[1];
+  // bilinearValues[2]= bilinearValues[2] < 0 ? 0 : bilinearValues[2];
+  //cout<< "Red: " << bilinearValues[0] << "\t Green: " << bilinearValues[1] << "\t Blue: " << bilinearValues[2] << "\n";
+  return P;
+
+  //return getPixelAt(x_value, y_value);
 }
+
 
 glm::dvec3 TextureMap::getPixelAt(int x, int y) const {
   // YOUR CODE HERE
@@ -151,7 +142,9 @@ glm::dvec3 TextureMap::getPixelAt(int x, int y) const {
   double dataR = data[index];
   double dataG = data[index+1];
   double dataB = data[index+2];
-  glm::dvec3 returnColor = glm::dvec3(dataR, dataG, dataB);
+  glm::dvec3 returnColor = glm::dvec3(dataR/255, dataG/255, dataB/255);
+
+  //cout<< "Red: " << returnColor[0] << "\t Green: " << returnColor[1] << "\t Blue: " << returnColor[2] << "\n";
   
   return returnColor;
 }
