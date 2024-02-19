472b93f2a0de10fbcdb0e3f70dafc2a8d4c5b440
kphang2 <karahphang@gmail.com>
2024-02-18 18:38:40 -0600

    kdtree header 'complete'

--- ray/scene/kdTree.cpp
+++ ray/scene/kdTree.cpp
@@ -1,2 +1,77 @@
 #include "kdTree.h"
+#include "scene.h"
+
+using namespace std;
+
+glm::dvec3 splitPlane::getPostition() {
+    return position;
+}
+
+glm::dvec3 splitPlane::getAxis() {
+    return axis;
+}
+
+void splitPlane::setPosition(glm::dvec3 p ){
+    position = p;
+}
+
+splitPlane findBestSplitPlane(std::vector <Geometry*> objList, BoundingBox bbox){
+    vector<splitPlane> candidates = vector<splitPlane>();
+    for (int axis = 0; axis < 3; axis++) {
+        for (Geometry* obj : objList) {
+            glm::dvec3 temp = glm::dvec3((0.0, 0.0, 0.0));
+            splitPlane sp1 = splitPlane(axis, temp);
+            splitPlane sp2 = splitPlane(axis, temp);
+            temp[axis] = obj->getBoundingBox().getMin()[axis];
+            sp1.setPosition(temp);
+            temp[axis] = obj->getBoundingBox().getMax()[axis];
+            sp2.setPosition(temp);
+            candidates.push_back(sp1);
+            candidates.push_back(sp2);
+            
+        }
+    }
+    double minsam = -1;
+    splitPlane ret = candidates[0];
+    for (splitPlane c : candidates) {
+        int leftCount = 0;
+        int rightCount = 0;
+        double leftbbox = 0;
+        double rightbbox = 0;
+        for (Geometry* obj : objList) {
+            if (obj->getBoundingBox().getMax()[c.getAxis()] <= c.getPostition()[c.getAxis()]) {
+                leftCount++;
+            }
+            if (obj->getBoundingBox().getMin()[c.getAxis()] >= c.getPostition()[c.getAxis()]) {
+                rightCount++;
+            }
+        }
+        glm::dvec3 p = c.getPostition();
+        for (double val = 0; val < 3; val++) {
+            if (val != c.getAxis()) {
+                p[val] = bbox.getMax()[val];
+            }
+        }
+        p = p - bbox.getMin();
+        leftbbox = glm::abs(p[0]*p[1]*p[2]);
+        p = c.getPostition();
+        for (double val = 0; val < 3; val++) {
+            if (val != c.getAxis()) {
+                p[val] = bbox.getMin()[val];
+            }
+        }
+        p = bbox.getMax() - p;
+        rightbbox = glm::abs(p[0]*p[1]*p[2]);
+
+        double sam = ((leftCount*leftbbox)+(rightCount*rightbbox))/bbox.volume();
+        if (minsam == -1 || sam < minsam) {
+            minsam = sam;
+            ret = c;
+        }
+        return ret;
+
+    }
+
+}
+
 
--- ray/scene/kdTree.h
+++ ray/scene/kdTree.h
@@ -4,48 +4,53 @@
 #pragma once
 #include <vector>
 #include <glm/vec3.hpp>
+#include "ray.h"
 class Geometry;
 class BoundingBox;
 // // Note: you can put kd-tree here
 
 
-class splitNode{
-    
-    splitNode *leftNode;
-    splitNode *rightNode;
+
+class splitPlane{
+    int axis;
+    glm::dvec3 position;
+
 public:
-    splitNode(splitNode *leftNode, splitNode *rightNode);
+    splitPlane(int axis, glm::dvec3 position);
+    glm::dvec3 getPostition();
+    void setPosition(glm::dvec3 p);
+    int getAxis();
+    int leftCount = 0 ;
+    int rightCount = 0;
 };
 
-class leafNode{
+class Node{
+    splitPlane plane;
+    Node *leftNode;
+    Node *rightNode;
     std::vector <Geometry*> objList;
 
 public:
-    leafNode(std::vector <Geometry*> objList);
-    std::vector <Geometry*> getObjectList() { return objList;}
+    Node(splitPlane plane, Node *leftNode, Node *rightNode, std::vector<Geometry*> objList);
+    std::vector <Geometry*> getObjectList() {return objList;}
     void addToObjectList(Geometry* shape) {objList.push_back(shape);}
-    
-};
-
-class splitPlane{
-public:
-    
-
+    bool findIntersectionSplit (ray &r, isect i, double tmin, double tmax);
+    bool findintersectionLeaf (ray &r, isect i, double tmin, double tmax);
 };
 
 class kdTree{
     int axis;
     glm::dvec3 position;
-    splitNode *root;
+    Node *root;
     std::vector <Geometry*> objList;
-
     int depthLimit = 5; // arbitrary limit rn
 
 public:
-    kdTree(int axis, glm::dvec3 position, splitNode *root, std::vector<Geometry*> objList);
-
-    leafNode buildTree(std::vector <Geometry*> objList, BoundingBox bbox, int depth, int leafSize);
+    kdTree(int axis, glm::dvec3 position, Node *root, std::vector<Geometry*> objList);
 };
+
+Node buildTree(std::vector <Geometry*> objList, BoundingBox bbox, int depth, int leafSize);
+splitPlane findBestSplitPlane(std::vector <Geometry*> objList, BoundingBox bbox);
 
 
 #endif
